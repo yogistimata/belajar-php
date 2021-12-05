35f2caf42634829288f575216b30c58750b005a6
yogistimata <sasmitayogi8@gmail.com>
2021-12-05 21:02:45 +0700

    taks 7

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php
+function salam($waktu , $mana)
+{
+    return "selamat $waktu , $nama!!!";
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viwport" content="width=device-width, initial-scale=1.0">
+    <title>latian function</title>
+</head>
+<body>
+<?php echo"<h1> SALAM (SORE, SAYA YOGI)</h1>";
+    ?>
+</body>
+</html>
